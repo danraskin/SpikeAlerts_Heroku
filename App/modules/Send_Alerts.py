05e053fa11c0a1321d2a67b5925e38f8bda9a249
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-12-05 01:00:53 -0600

    Corrected Last Messaged
    This code corrects the last messaged error

--- App/modules/Send_Alerts.py
+++ App/modules/Send_Alerts.py
@@ -64,8 +64,7 @@ def send_all_messages(record_ids, messages, redCap_token_signUp, pg_connection_d
     times = our_twilio.send_texts(numbers, messages) # See twilio_functions.py
     
     update_user_table(record_ids, times, pg_connection_dict) # See Send_Alerts.py
-
-    return
+    
 
 # ~~~~~~~~~~~~~
 
@@ -74,49 +73,61 @@ def update_user_table(record_ids, times, pg_connection_dict):
     Takes a list of users + time objects and updates the "Sign Up Information" table
     to increment each user's messages_sent and last_messaged
     '''
-    #print("updating Sign Up Information", record_ids, times)
-
-    conn = psycopg2.connect(**pg_connection_dict,keepalives_idle=10)
-    cur = conn.cursor()
-
-    # dataframe is sorted by record ID because SQL messages_sent query needs to be ordered (and this needs to match
-    sorted = pd.DataFrame({'record_ids': record_ids,'times': times}).sort_values(by = "record_ids")  
 
     cmd = sql.SQL('''
-    SELECT messages_sent
-    FROM "Sign Up Information" u
-    WHERE u.record_id = ANY ( {} )
-    ORDER BY u.record_id asc; 
-    ''').format(sql.Literal(record_ids))
+    UPDATE "Sign Up Information"
+    SET last_messaged = CURRENT TIMESTAMP AT TIME ZONE 'America/Chicago', messages_sent = messages_sent + 1
+    WHERE record_id = ANY ( {} );
+    ''').format(sql.Literal(record_ids)
+                )
+    psql.send_update(cmd, pg_connection_dict)
+    
+    # Old version
+    
+    #print("updating Sign Up Information", record_ids, times)
 
-    cur.execute(cmd)
-    conn.commit()
+#    conn = psycopg2.connect(**pg_connection_dict,keepalives_idle=10)
+#    cur = conn.cursor()
 
-    messages_sent_list = [i[0] for i in cur.fetchall()] # Unpack results into list
-    messages_sent_new = [v+1 for v in messages_sent_list]
-    sorted["messages_sent_new"] = messages_sent_new
+#    # dataframe is sorted by record ID because SQL messages_sent query needs to be ordered (and this needs to match
+#    sorted = pd.DataFrame({'record_ids': record_ids,'times': times}).sort_values(by = "record_ids")  
+
+#    cmd = sql.SQL('''
+#    SELECT messages_sent
+#    FROM "Sign Up Information" u
+#    WHERE u.record_id = ANY ( {} )
+#    ORDER BY u.record_id asc; 
+#    ''').format(sql.Literal(record_ids))
+
+#    cur.execute(cmd)
+#    conn.commit()
+
+#    messages_sent_list = [i[0] for i in cur.fetchall()] # Unpack results into list
+#    messages_sent_new = [v+1 for v in messages_sent_list]
+#    sorted["messages_sent_new"] = messages_sent_new
 
     # SQL statement that updates each record (identified by record_ids) with new times, messages_sent_new values
     # if this ever has performance trouble, we could try https://dev.mysql.com/doc/refman/8.0/en/insert-on-duplicate.html
     # which would require record_id to be made into a foreign key  
-    for id, time, msg_inc in zip(sorted["record_ids"], sorted["times"], sorted["messages_sent_new"]):
-        cmd = sql.SQL('''
-        UPDATE "Sign Up Information"
-        SET last_messaged = {lm}, messages_sent = {ms} 
-        WHERE record_id =  {ri} ;
-        ''').format(ri = sql.Literal(id),
-                    lm = sql.Literal(time),
-                    ms = sql.Literal(msg_inc)
-                    )
-        cur.execute(cmd)
+#    for id, time, msg_inc in zip(sorted["record_ids"], sorted["times"], sorted["messages_sent_new"]):
+#        cmd = sql.SQL('''
+#        UPDATE "Sign Up Information"
+#        SET last_messaged = CURRENT TIMESTAMP AT TIME ZONE 'America/Chicago', messages_sent = messages_sent + 1
+#        WHERE record_id = ANY ( {ri} );
+#        ''').format(ri = sql.Literal(id),
+#                    lm = sql.Literal(time),
+#                    ms = sql.Literal(msg_inc)
+#                    )
+#        cur.execute(cmd)
     
-    conn.commit()
+#    conn.commit()
     
-    cur.close()
-    conn.close()
-    return
+ #   cur.close()
+#    conn.close()
     
 # ~~~~~~~~~~~~~
+
+
     
 def Unsubscribe_users(record_ids, pg_connection_dict):
     '''
